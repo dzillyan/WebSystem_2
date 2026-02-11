92cec0b9c2ea9679822c232de0130aeadc3b3a71
Gellie Anne Costales <gilleecstls@gmail.com>
2026-02-11 23:18:16 +0800

    added

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 2</title>
+
+    <style>
+        h3{
+            color: blue;
+            font-size:100px;
+        }
+    </style>
+</head>
+<body>
+    <h3>Gellie Anne Costales</h3>
+</body>
+</html>
